0b91529aa247adba370e2aed9653f806101163ae
noa <noataso1127@gmail.com>
2024-08-06 02:53:27 +0900

    noa_test

--- hello_world.py
+++ hello_world.py
@@ -3,3 +3,7 @@ print("Hello World!")
 # ==== ayaka add ====
 name = input("please input your name > ")
 print(f"Hello, {name}")
+
+# ==== noa add ====
+age = input("please input your age > ")
+print(f"Hello, {name}({age})")
